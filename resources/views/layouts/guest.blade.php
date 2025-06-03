de7a23d58a0893e52e20750be446d6a497c44527
alexaaqil <alexaaqil.se@gmail.com>
2025-06-04 02:22:18 +0300

    modified the stylesheets to be more specific about the layout.

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+        <!-- Scripts / Styles -->
+        @vite('resources/css/guest-layout.css')
+
+        @isset($extra_head)
+            {{ $extra_head }}
+        @else
+            <title>Qing Jia Safaris</title>
+        @endisset
+    </head>
+    <body class="antialiased font-sans">
+        <livewire:partials.navbar />
+
+        <main class="guest_layout">
+            {{ $slot }}
+        </main>
+
+        @isset($javascript)
+            {{ $javascript }}
+        @endisset
+    </body>
+</html>
