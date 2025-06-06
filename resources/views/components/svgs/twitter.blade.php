809574f3cc7360c07210f9616e79ccd8e8ecc023
alexaaqil <alexaaqil.se@gmail.com>
2025-06-06 16:09:29 +0300

    corrected the class attribute for the svgs.

--- resources/views/components/svgs/twitter.blade.php
+++ resources/views/components/svgs/twitter.blade.php
@@ -1,6 +1,6 @@
 <!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {{ $attributes->merge(['class' => 'w-6 h-6']) }}>
   <path d="M18.73 2H5.27A3.27 3.27 0 0 0 2 5.27v13.46A3.27 3.27 0 0 0 5.27 22h13.46A3.27 3.27 0 0 0 22 18.73V5.27A3.27 3.27 0 0 0 18.73 2ZM16.7 16.48h-2.1l-2.59-3.26-2.6 3.26H7.3l3.62-4.51L7.3 7.52h2.11l2.59 3.25 2.6-3.25h2.1l-3.63 4.45 3.63 4.51Z"/>
 </svg> -->
-<svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
+<svg fill="none" stroke="currentColor" viewBox="0 0 24 24" {{ $attributes->merge(['class' => 'w-5 h-5']) }}>
     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
 </svg>
