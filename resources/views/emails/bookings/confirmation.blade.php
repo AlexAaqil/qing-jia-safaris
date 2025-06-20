9fdca1201ca54aa6e3c7fd827334863158ea7196
alexaaqil <alexaaqil.se@gmail.com>
2025-06-20 22:20:02 +0300

    corrected the markdown file for the booking confirmation.

--- resources/views/emails/bookings/confirmation.blade.php
+++ resources/views/emails/bookings/confirmation.blade.php
@@ -7,14 +7,11 @@ Thank you for booking with us. Here are your booking details:
 
 <p><strong>Booking Code:</strong> {{ $booking->booking_code }}</p>
 <p><strong>Tour Name:</strong> {{ $booking->tour->title }}</p>
-<p><strong>Travel Date:</strong> {{ $booking->date_of_travel ?? 'Not specified' }}</p>
+<p><strong>Travel Date:</strong> {{ $booking->date_of_travel->format('jS M Y') ?? 'Not specified' }}</p>
 <p><strong>Adults:</strong> {{ $booking->number_of_adults }}</p>
 <p><strong>Children:</strong> {{ $booking->number_of_children }}</p>
 
-@if ($booking->additional_information)
-    <p><strong>Additional Notes:</strong></p>
-    <p>{{ $booking->additional_information }}</p>
-@endif
+<p><strong>Additional Notes:</strong> {{ $booking->additional_information ?? 'N/A' }}</p>
 
 @component('mail::button', ['url' => route('book-tour-success', $booking->uuid)])
 View Your Booking
