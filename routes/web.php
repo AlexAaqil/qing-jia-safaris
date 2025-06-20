b917654b761a8393ac02f89f5c78a1bc4aecc274
alexaaqil <alexaaqil.se@gmail.com>
2025-06-21 00:05:13 +0300

    updated the routes for authorization of staff and admin pages.

--- routes/web.php
+++ routes/web.php
@@ -36,33 +36,35 @@ Route::get('tours/book/{tour}', BookTour::class)->name('book-tour');
 Route::get('tours/booking-status/{booking}', BookTourSuccess::class)->name('book-tour-success');
 
 Route::middleware(['authenticated_user'])->group(function() {
-    Route::get('dashboard', Dashboard::class)->name('dashboard');
+    Route::prefix('staff')->group(function() {
+        Route::get('dashboard', Dashboard::class)->name('dashboard');
 
-    Route::get('tour-categories', TourCategories::class)->name('tour-categories.index');
-    Route::get('tour-categories/create', [TourCategoryController::class, 'create'])->name('tour-categories.create');
-    Route::post('tour-categories', [TourCategoryController::class, 'store'])->name('tour-categories.store');
-    Route::get('tour-categories/{tour_category}/edit', [TourCategoryController::class, 'edit'])->name('tour-categories.edit');
-    Route::patch('tour-categories/{tour_category}', [TourCategoryController::class, 'update'])->name('tour-categories.update');
+        Route::get('tour-categories', TourCategories::class)->name('tour-categories.index');
+        Route::get('tour-categories/create', [TourCategoryController::class, 'create'])->name('tour-categories.create');
+        Route::post('tour-categories', [TourCategoryController::class, 'store'])->name('tour-categories.store');
+        Route::get('tour-categories/{tour_category}/edit', [TourCategoryController::class, 'edit'])->name('tour-categories.edit');
+        Route::patch('tour-categories/{tour_category}', [TourCategoryController::class, 'update'])->name('tour-categories.update');
 
-    Route::get('tours', Tours::class)->name('tours.index');
-    Route::get('tours/create', [TourController::class, 'create'])->name('tours.create');
-    Route::post('tours', [TourController::class, 'store'])->name('tours.store');
-    Route::get('tours/{tour}/edit', [TourController::class, 'edit'])->name('tours.edit');
-    Route::patch('tours/{tour}', [TourController::class, 'update'])->name('tours.update');
+        Route::get('tours', Tours::class)->name('tours.index');
+        Route::get('tours/create', [TourController::class, 'create'])->name('tours.create');
+        Route::post('tours', [TourController::class, 'store'])->name('tours.store');
+        Route::get('tours/{tour}/edit', [TourController::class, 'edit'])->name('tours.edit');
+        Route::patch('tours/{tour}', [TourController::class, 'update'])->name('tours.update');
 
-    Route::get('tour-destinations', TourDestinations::class)->name('tour-destinations.index');
-    Route::get('tour-destinations/create', [DestinationController::class, 'create'])->name('tour-destinations.create');
-    Route::post('tour-destinations', [DestinationController::class, 'store'])->name('tour-destinations.store');
-    Route::get('tour-destinations/{destination}/edit', [DestinationController::class, 'edit'])->name('tour-destinations.edit');
-    Route::patch('tour-destinations/{destination}', [DestinationController::class, 'update'])->name('tour-destinations.update');
+        Route::get('tour-destinations', TourDestinations::class)->name('tour-destinations.index');
+        Route::get('tour-destinations/create', [DestinationController::class, 'create'])->name('tour-destinations.create');
+        Route::post('tour-destinations', [DestinationController::class, 'store'])->name('tour-destinations.store');
+        Route::get('tour-destinations/{destination}/edit', [DestinationController::class, 'edit'])->name('tour-destinations.edit');
+        Route::patch('tour-destinations/{destination}', [DestinationController::class, 'update'])->name('tour-destinations.update');
 
-    Route::delete('tour-images/{tour_image}', [TourImageController::class, 'destroy'])->name('tour-images.destroy');
-    Route::post('tour-images/sort', [TourImageController::class, 'sort'])->name('tour-images.sort');
+        Route::delete('tour-images/{tour_image}', [TourImageController::class, 'destroy'])->name('tour-images.destroy');
+        Route::post('tour-images/sort', [TourImageController::class, 'sort'])->name('tour-images.sort');
 
-    Route::get('bookings', Bookings::class)->name('bookings.index');
-    Route::get('bookings/{booking}/edit', EditBookings::class)->name('bookings.edit');
+        Route::get('bookings', Bookings::class)->name('bookings.index');
+        Route::get('bookings/{booking}/edit', EditBookings::class)->name('bookings.edit');
 
-    Route::get('contact-messages', ContactMessages::class)->name('contact-messages.index');
+        Route::get('contact-messages', ContactMessages::class)->name('contact-messages.index');
+    });
 });
 
 Route::middleware(['admin_only'])->group(function() {
