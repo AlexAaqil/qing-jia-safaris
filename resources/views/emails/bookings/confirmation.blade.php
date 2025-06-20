aa87842abe529ba038ce6a4d990c12f99974b332
alexaaqil <alexaaqil.se@gmail.com>
2025-06-20 16:17:01 +0300

    updated the structure of the confirmation email.

--- resources/views/emails/bookings/confirmation.blade.php
+++ resources/views/emails/bookings/confirmation.blade.php
@@ -5,15 +5,15 @@ Hello {{ $booking->first_name }},
 
 Thank you for booking with us. Here are your booking details:
 
-**Booking Code:** {{ $booking->booking_code }}
-**Tour Name:** {{ $booking->tour->title }}
-**Travel Date:** {{ $booking->date_of_travel ?? 'Not specified' }}
-**Adults:** {{ $booking->number_of_adults }}
-**Children:** {{ $booking->number_of_children }}
+<p><strong>Booking Code:</strong> {{ $booking->booking_code }}</p>
+<p><strong>Tour Name:</strong> {{ $booking->tour->title }}</p>
+<p><strong>Travel Date:</strong> {{ $booking->date_of_travel ?? 'Not specified' }}</p>
+<p><strong>Adults:</strong> {{ $booking->number_of_adults }}</p>
+<p><strong>Children:</strong> {{ $booking->number_of_children }}</p>
 
 @if ($booking->additional_information)
-**Additional Notes:**
-{{ $booking->additional_information }}
+    <p><strong>Additional Notes:</strong></p>
+    <p>{{ $booking->additional_information }}</p>
 @endif
 
 @component('mail::button', ['url' => route('book-tour-success', $booking->uuid)])
