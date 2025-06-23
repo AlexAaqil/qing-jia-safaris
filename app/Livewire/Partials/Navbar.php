bb12305f11f2d18c1a073f71257e3028cd5a1ed8
alexaaqil <alexaaqil.se@gmail.com>
2025-06-23 22:36:25 +0300

    added functionality for a drop down on the navbar for tour categories.

--- app/Livewire/Partials/Navbar.php
+++ app/Livewire/Partials/Navbar.php
@@ -4,9 +4,17 @@ namespace App\Livewire\Partials;
 
 use Livewire\Component;
 use App\Livewire\Actions\Auth\Logout;
+use App\Models\Tours\TourCategory;
 
 class Navbar extends Component
 {
+    public $tour_categories;
+
+    public function mount()
+    {
+        $this->tour_categories = TourCategory::take(10)->get();
+    }
+
     public function logout(Logout $logout)
     {
         $logout();
